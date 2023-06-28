3d0fb73c3856c5a38fe2265c67ff733107998ad9
David Aguirre <david.aguirreq@perficient.com>
2023-06-28 09:19:06 -0500

    daguirre/09-generate-static-file-information

---
+++ model/pokeNature.ts
@@ -0,0 +1,7 @@
+export interface PokemonNature {
+  id: string;
+  name: string;
+  description: string;
+  increaseStat: string;
+  decreaseStat: string;
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    pokeapiv2_URL: "https://pokeapi.co/api/v2/"
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
